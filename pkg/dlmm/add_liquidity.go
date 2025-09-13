5e34d76d87cac3672f620875c2db678e11d9830a
winit <heywinit@gmail.com>
2025-09-13 11:20:27 +0530

    fixing the fucking errors someone commited (fu deep)

--- pkg/dlmm/add_liquidity.go
+++ pkg/dlmm/add_liquidity.go
@@ -2,12 +2,9 @@ package dlmm
 
 import (
 	"context"
-	"log"
-	"math"
-	"math/big"
 
 	"github.com/gagliardetto/solana-go"
-	"github.com/gagliardetto/solana-go/"
+
 	lb_clmm "github.com/metfin/dlmm-sdk-go/pkg/generated"
 )
 
@@ -24,11 +21,14 @@ const (
 	Curve
 	BidAsk
 )
-func getAssociatedTokenAddressSync(mint solana.PublicKey , owner solana.PublicKey , allowOwnerOffCurve bool) (solana.PublicKey , error ) {
-	if (!allowOwnerOffCurve && !solana.PublicKey.IsOnCurve()){
-		return 
-	}
-}
+
+// func getAssociatedTokenAddressSync(mint solana.PublicKey , owner solana.PublicKey , allowOwnerOffCurve bool) (solana.PublicKey , error ) {
+// 	if (!allowOwnerOffCurve && !solana.PublicKey.IsOnCurve()){
+// 		return 
+// 	}
+// }
+
+
 func (c *Client) createBinArraysIfNeeded(binaArrayIndex []int64, funder solana.PublicKey, lbpair solana.PublicKey) ([]solana.Instruction, error) {
 	var ixs []solana.Instruction
 	for _, idx := range binaArrayIndex {
@@ -67,7 +67,7 @@ func getBinArrayIndexesCoverage(lowerBinId, upperBinId int32) []int64 {
 	return binArrayIndexes
 }
 
-func getBinArrayAccountMetasCoverage(lowerBinId, upperBinId int32, lbpair solana.PublicKey, programID solana.PublicKey) ([]solana.AccountMeta, error) {
+func getBinArrayAccountMetasCoverage(lowerBinId, upperBinId int32, lbpair solana.PublicKey, programID solana.PublicKey) ([]*solana.AccountMeta, error) {
 	binArrayKeyscoverage, err := getBinArrayKeyCoverage(lowerBinId, upperBinId, lbpair, programID)
 	if err != nil {
 		return nil, err
@@ -81,6 +81,7 @@ func getBinArrayAccountMetasCoverage(lowerBinId, upperBinId int32, lbpair solana
 		})
 
 	}
+	return accountMetaData, nil
 }
 
 func getBinArrayKeyCoverage(lowerBinId, upperBinId int32, lbpair solana.PublicKey, programID solana.PublicKey) ([]solana.PublicKey, error) {
@@ -100,54 +101,50 @@ func getBinArrayKeyCoverage(lowerBinId, upperBinId int32, lbpair solana.PublicKe
 }
 
 
-func (c *Client) getorCreateATAInstruction(tokenMint solana.PublicKey , owner solana.PublicKey) {
-	toAccount := 
-}
-func (c *Client) initializePositionAndAddLiquidityByStrategy(ctx context.Context, lbPairPubKey solana.PublicKey, positionPubKey solana.PublicKey, totalXamount big.Int, totalYamount big.Int, strategy StrategyParameters, user solana.PublicKey, slippage int) (*[]solana.Transaction, error) {
-	maxBinId := strategy.maxBinId
-	minBinId := strategy.minBinId
-	lbpair, err := lb_clmm.ParseAccount_LbPair(lbPairPubKey[:])
-	if err != nil {
-		log.Print("error parsing lbpair account %s", lbPairPubKey)
-		return nil, err
-	}
-	width := maxBinId - maxBinId + 1
-	var maxActiveBinSlippage int
-	if slippage != 0 {
-		maxActiveBinSlippage = int(math.Ceil(
-			float64(slippage) / (float64(lbpair.BinStep) / 100.0),
-		))
-	} else {
-		maxActiveBinSlippage = dlmm.MaxActiveBinSlippage
-	}
-	var preInstructions []solana.Instruction
+// func (c *Client) initializePositionAndAddLiquidityByStrategy(ctx context.Context, lbPairPubKey solana.PublicKey, positionPubKey solana.PublicKey, totalXamount big.Int, totalYamount big.Int, strategy StrategyParameters, user solana.PublicKey, slippage int) (*[]solana.Transaction, error) {
+// 	maxBinId := strategy.maxBinId
+// 	minBinId := strategy.minBinId
+// 	lbpair, err := lb_clmm.ParseAccount_LbPair(lbPairPubKey[:])
+// 	if err != nil {
+// 		log.Print("error parsing lbpair account %s", lbPairPubKey)
+// 		return nil, err
+// 	}
+// 	width := maxBinId - maxBinId + 1
+// 	// if slippage != 0 {
+// 	// 	maxActiveBinSlippage = int(math.Ceil(
+// 	// 		float64(slippage) / (float64(lbpair.BinStep) / 100.0),
+// 	// 	))
+// 	// } else {
+// 	// 	maxActiveBinSlippage = dlmm.MaxActiveBinSlippage
+// 	// }
+// 	var preInstructions []solana.Instruction
 
-	initializePositionIx, err := lb_clmm.NewInitializePositionInstruction(
-		minBinId,
-		width,
-		user,
-		positionPubKey,
-		lbPairPubKey,
-		user,
-		SystemProgramAccount,
-		user,
-		EventAuthorityAccount,
-		MainnetProgramID,
-	)
-	if err != nil {
-		log.Printf("error initializing position")
-		return nil, err
-	}
-	preInstructions = append(preInstructions, initializePositionIx)
+// 	initializePositionIx, err := lb_clmm.NewInitializePositionInstruction(
+// 		minBinId,
+// 		width,
+// 		user,
+// 		positionPubKey,
+// 		lbPairPubKey,
+// 		user,
+// 		SystemProgramAccount,
+// 		user,
+// 		EventAuthorityAccount,
+// 		MainnetProgramID,
+// 	)
+// 	if err != nil {
+// 		log.Printf("error initializing position")
+// 		return nil, err
+// 	}
+// 	preInstructions = append(preInstructions, initializePositionIx)
 
-	binArrayIndexes := getBinArrayIndexesCoverage(minBinId, maxBinId)
+// 	binArrayIndexes := getBinArrayIndexesCoverage(minBinId, maxBinId)
 
-	binArrayAccountMetaData, err := getBinArrayAccountMetasCoverage(minBinId, maxBinId, lbPairPubKey, MainnetProgramID)
-	if err != nil {
-		log.Printf("error getting bin array metadata")
-		return nil, err
-	}
-	createBinarrayIxs , err := c.createBinArraysIfNeeded(binArrayIndexes , user ,lbPairPubKey)
-	preInstructions = append(preInstructions, createBinarrayIxs...)
-
-}
+// 	_, err = getBinArrayAccountMetasCoverage(minBinId, maxBinId, lbPairPubKey, MainnetProgramID)
+// 	if err != nil {
+// 		log.Printf("error getting bin array metadata")
+// 		return nil, err
+// 	}
+// 	createBinarrayIxs , err := c.createBinArraysIfNeeded(binArrayIndexes , user ,lbPairPubKey)
+// 	preInstructions = append(preInstructions, createBinarrayIxs...)
+// 	return &preInstructions, nil
+// }
